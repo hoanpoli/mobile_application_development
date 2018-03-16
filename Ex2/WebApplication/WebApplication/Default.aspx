e78a5173e52e9da44688954f60d83644fba65080
Hoan Nguyen <nvhoanbk14@gmail.com>
2018-03-17 00:51:21 +0700

    add exercise 2

---
+++ Ex2/WebApplication/WebApplication/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master"
+    AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication._Default" %>
+
+
+
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>EX2</h2>
+    <div style="border : 1px solid red ; width:300px">
+        <p>Họ và tên : Nguyễn Văn Hoan</p>
+        <p>MSSV : 1411275</p>
+    </div>
+    <form method="POST" id="f1">
+        Latitude 1:
+        <input id="lat1" type="number" />
+        <br />
+        Longitude 1:
+        <input id="long1" type="number" />
+        <br />
+        Latitude 2:
+        <input id="lat2" type="number" />
+        <br />
+        Longitude 2:
+        <input id="long2" type="number" />
+        <br />
+        <button id="tong" onclick="abc()">Khoang cach</button>
+
+    </form>
+    <script>
+    $("#f1").submit(function() {
+        doSomething(); 
+    });
+    </script>
+    <br />
+    <script src="Scripts/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+
+       
+            function abc() {
+                var a = $("#lat1").val();
+                var b = $("#long1").val();
+                var c = $("#lat2").val();
+                var d = $("#long2").val();
+
+                var url = "api/Tong" + "?Lat1=" + a + "&Long1="+b+"&Lat2="+c + "&Long2="+d;
+                $.getJSON(url,
+                    function (data) {
+                        alert("Khỏang cách 2 điểm là : " + data + " km");
+                    });
+            }
+
+    </script>
+</asp:Content>
---
+++ Ex2/WebApplication/WebApplication/TongController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication
+{
+    public class TongController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<int> Get()
+        {
+            return new int[] {3,4 , 4,4,2,13,3,4,2};
+        }
+
+        // GET api/<controller>/5
+        public double Get(double Lat1, 
+                  double Long1, double Lat2, double Long2)
+        {
+            double dDistance = Double.MinValue;
+            double dLat1InRad = Lat1 * (Math.PI / 180.0);
+            double dLong1InRad = Long1 * (Math.PI / 180.0);
+            double dLat2InRad = Lat2 * (Math.PI / 180.0);
+            double dLong2InRad = Long2 * (Math.PI / 180.0);
+
+            double dLongitude = dLong2InRad - dLong1InRad;
+            double dLatitude = dLat2InRad - dLat1InRad;
+
+            // Intermediate result a.
+            double a = Math.Pow(Math.Sin(dLatitude / 2.0), 2.0) +
+                       Math.Cos(dLat1InRad) * Math.Cos(dLat2InRad) *
+                       Math.Pow(Math.Sin(dLongitude / 2.0), 2.0);
+
+            // Intermediate result c (great circle distance in Radians).
+            double c = 2.0 * Math.Asin(Math.Sqrt(a));
+
+            // Distance.
+            // const Double kEarthRadiusMiles = 3956.0;
+            const Double kEarthRadiusKms = 6376.5;
+            dDistance = kEarthRadiusKms * c;
+
+            return dDistance;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
